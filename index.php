a6e3746efddcf0bb8d9eb327da34cd4656151580
Daniel Koo <dkoo.dev@gmail.com>
2016-11-12 02:20:59 -0800

    Google Maps Placement
    Google maps works

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="main.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/google-maps.css">
+
 		<title>Travel App</title>
 	</head>
 
 	<body>
-		
+    <input id="pac-input" class="controls" type="text"
+        placeholder="Enter a location">
+    <div id="map"></div>
 	</body>
-<script type="text/javascript" src="script.js" ></script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaopJNj_frN6fwXANNEKsK6LByLqdaZIA&libraries=places&callback=initMap" async defer></script>
+<script type="text/javascript" src="js/script.js" ></script>
+<script type="text/javascript" scr="js/google-maps.js"></script>
 </html>
