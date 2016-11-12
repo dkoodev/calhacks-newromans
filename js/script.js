54902a15b97d9b6c2d6381730b3df297aade5998
Nishir <nishelat@yahoo.com>
2016-11-12 05:11:41 -0800

    Implement random picture selector

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var destination;
 var start;
 
+var index = Math.floor(Math.random() * 10);
+index = 'url(../pictures/' + index + '.jpg)';
+document.body.style.backgroundImage = index;
+
+
 document.getElementById('destinationButton').addEventListener('click', function() {
 	document.getElementById('destination').style.display = "none";
 	destination = document.getElementById('destinationInput').value;
