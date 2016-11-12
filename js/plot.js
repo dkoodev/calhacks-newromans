bc0fb6de5d74f41f9316b2c5899637b04b52d299
Kun Peng <kunpeng@usc.edu>
2016-11-12 06:03:06 -0800

    plot.js
    plot.js

---
+++ js/plot.js
@@ -0,0 +1,59 @@
+var rawDataURL = '../resources/output.csv';
+var xField = 'Departure Date';
+var yField = 'Price';
+
+var selectorOptions = {
+    buttons: [{
+        step: 'month',
+        stepmode: 'backward',
+        count: 1,
+        label: '1 Month'
+    }, {
+        step: 'month',
+        stepmode: 'backward',
+        count: 6,
+        label: '6 Month'
+    }, {
+        step: 'year',
+        stepmode: 'backward',
+        count: 1,
+        label: '1 year'
+    }, {
+        step: 'all',
+    }],
+};
+
+Plotly.d3.csv(rawDataURL, function(err, rawData) {
+    if(err) throw err;
+
+    var data = prepData(rawData);
+    var layout = {
+        xaxis: {
+            rangeselector: selectorOptions,
+            rangeslider: {}
+        },
+        yaxis: {
+            title: 'Price',
+            fixedrange: true
+        }
+    };
+
+    Plotly.plot('graph', data, layout);
+});
+
+function prepData(rawData) {
+    var x = [];
+    var y = [];
+
+    rawData.forEach(function(datum, i) {
+
+        x.push(new Date(datum[xField]));
+        y.push(datum[yField]);
+    });
+
+    return [{
+        mode: 'lines',
+        x: x,
+        y: y
+    }];
+}
