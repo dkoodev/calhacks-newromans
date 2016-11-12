e3c731dff0a4d05524c3394ad7d7d93a25ff2829
Daniel Koo <dkoo.dev@gmail.com>
2016-11-12 07:42:34 -0800

    Search box autocomlete feature

--- index.php
+++ index.php
@@ -13,7 +13,14 @@
 			$output = shell_exec($command);
 		?>
 	<div id="history"><br></div>
-	<div id="destination"><div id="destinationPrompt">Where would you like to go?</div><input type="text" id="destinationInput" placeholder="E.g. New York City"><div id="destinationButton">Go</div></div>
+	<div id="destination">
+		<div id="destinationPrompt">Where would you like to go?</div>
+			<!-- <input type="text" id="destinationInput" placeholder="E.g. New York City"> -->
+
+			<input id="pac-input" class="controls" type="text" placeholder="Search Box">
+
+			<div id="destinationButton">Go</div>
+		</div>
 	<div id="start"><div id="startPrompt">Where are you starting from?</div><input type="text" id="startInput" placeholder="E.g. University of Southern California"><div id="startButton">Go</div></div>
     <div id="map"></div>
     <div id="tester"> </div> 
@@ -25,5 +32,4 @@
 <script type="text/javascript" src="js/google-maps.js"></script>
 <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaopJNj_frN6fwXANNEKsK6LByLqdaZIA&libraries=places&callback=initMap" async defer></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaopJNj_frN6fwXANNEKsK6LByLqdaZIA&libraries=places"></script>
 </html>
