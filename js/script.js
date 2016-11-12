08144c8a8c1443367df5af7dad96297af981f3c9
Nishir <nishelat@yahoo.com>
2016-11-12 15:39:10 -0800

    Remove the ADT

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var destination;
 var start;
 
 
-var index = Math.floor(Math.random() *19);
+var index = Math.floor(Math.random() *18);
 var index_copy = index;
 index = 'url(../pictures/' + index + '.jpg)';
 document.body.style.backgroundImage = index;
@@ -20,12 +20,11 @@ var locations = ["E.g. Grand Canyon, United States"
 , "E.g. La Fortuna, Costa Rica"
 , "E.g. Desert Safari, Dubai"
 , "E.g. Taipei, Taiwan"
-, "E.g. Mexico Beach, United State"
+, "E.g. Mexico Beach, United States"
 , "E.g. Trier, Germany"
 , "E.g. Halsingland, Sweden"
 , "E.g. Yosemite, United States"
-, "E.g. Jarrettsville, United States"
-, "E.g. Arc de Triomphe, France"]
+, "E.g. Jarrettsville, United States"]
 
 var curr_loc = locations[index_copy];
 document.getElementById('destinationInput').placeholder = curr_loc;
@@ -41,7 +40,7 @@ document.getElementById('destinationButton').addEventListener('click', function(
 
 document.getElementById('switchButtonLeft').addEventListener('click', function() {
 	if(index_copy == 0) {
-		index_copy = 18;
+		index_copy = 17;
 	}
 	else {
 		index_copy--;
@@ -53,7 +52,7 @@ document.getElementById('switchButtonLeft').addEventListener('click', function()
 });
 
 document.getElementById('switchButtonRight').addEventListener('click', function() {
-	if(index_copy == 18) {
+	if(index_copy == 17) {
 		index_copy = 0;
 	}
 	else {
