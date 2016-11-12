a779e9347b99f0184bd142094a40b57e4decdff7
Daniel Koo <dkoo.dev@gmail.com>
2016-11-12 01:57:52 -0800

    Major update on google maps implementaion

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="main.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="css/google-maps.css">
 		<title>Travel App</title>
 	</head>
 
 	<body>
-		
+		<input id="pac-input" class="controls" type="text"
+		    placeholder="Enter a location">
+		<div id="map"></div>
+
 	</body>
-<script type="text/javascript" src="script.js" ></script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaopJNj_frN6fwXANNEKsK6LByLqdaZIA&libraries=places&callback=initMap"
+	    async defer></script>
+<!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCaopJNj_frN6fwXANNEKsK6LByLqdaZIA&libraries=places"></script> -->
+<script type="text/javascript" src="js/script.js" ></script>
+<script type="text/javascript" src="js/google-maps.js"></script>
 </html>
